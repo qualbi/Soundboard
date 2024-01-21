92ae4d098098f6961c4f36246c84299170bc8ca9
qualbi <kevqbui6@gmail.com>
2024-01-20 22:46:58 -0500

    Initial Commit

---
+++ Xcode Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xcode Demo
+//
+//  Created by Kevin Bui on 1/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Kevin Bui on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
